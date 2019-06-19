700e7e9c1c7d955791d8b52cc344682a487f9469
Adam Cupiał <adam.cupial@dazn.com>
2019-06-19 23:31:03 +0200

    remove: font-awesome script, use just icons instead

--- src/widgets/header/script.ts
+++ src/widgets/header/script.ts
@@ -1,27 +1,14 @@
 import './style.scss';
 import BaseWidget from 'scripts/base-widget';
-import { library, dom, config } from '@fortawesome/fontawesome-svg-core';
-import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons'
-import { faPython, faCss3Alt, faJs, faHtml5 } from '@fortawesome/free-brands-svg-icons'
 
 
 export default class HeaderWidget extends BaseWidget {
-  private loadSVG () : void {
-    config.autoAddCss = false;
-    library.add(faHtml5, faPython, faCss3Alt, faJs, faSearch, faBars);
-    dom.i2svg({
-      node: this.context as Node,
-      callback: () => {},
-    });
-  }
-
   render() : void {
     this.context
-    .querySelector('.navigation-button')
-    .addEventListener('click', ({ currentTarget }) => {
-      currentTarget.classList.toggle('active');
-      document.documentElement.classList.toggle('no-scroll');
-    }, false);
-    this.loadSVG();
+      .querySelector('.navigation-button')
+      .addEventListener('click', ({ currentTarget }) => {
+        currentTarget.classList.toggle('active');
+        document.documentElement.classList.toggle('no-scroll');
+      }, false);
   }
 }
